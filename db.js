a0a49f710b2e1ed4b446d9115300914b52e79a02
Monalisa Das <nityanidas2019@gmail.com>
2025-08-04 11:55:16 +0530

    changes to upgrade postgreSQL DB

--- db.js
+++ db.js
@@ -1,7 +1,11 @@
-import mysql from 'mysql2';
-export const db = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'root',
-  database: 'linkedin_clone'
+import pkg from 'pg';
+const { Pool } = pkg;
+
+export const db = new Pool({
+  host: process.env.DB_HOST || 'localhost',
+  user: process.env.DB_USER || 'postgres',
+  password: process.env.DB_PASS || '',
+  database: process.env.DB_NAME || 'linkedin_clone',
+  port: process.env.DB_PORT || 5432,
+  ssl: process.env.DB_SSL === 'true' // Render PostgreSQL requires SSL
 });
